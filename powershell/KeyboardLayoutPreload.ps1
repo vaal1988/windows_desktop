178ab148d13cda20f6ec2ea075a00d8a733f663a
Gusachenko Valentin <vgusachenko@gmail.com>
2020-12-30 14:22:14 +0200

    LanguageList

--- powershell/KeyboardLayoutPreload.ps1
+++ powershell/KeyboardLayoutPreload.ps1
@@ -1,5 +1,5 @@
 REG LOAD HKLM\DEFAULT C:\Users\Default\NTUSER.DAT
 
-reg add "HKLM\DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v LanguageList /t REG_SZ /d 'powershell -command "Set-WinUserLanguageList -LanguageList en-US, ru, uk -Force"' /f
+reg add "HKLM\DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v LanguageList /t REG_SZ /d "powershell -command 'Set-WinUserLanguageList -LanguageList en-US, ru, uk -Force'" /f
 
 REG UNLOAD HKLM\DEFAULT
