28572a192ce269479375a8f5a54af6ef8b1ef388
vaal1988 <45043510+vaal1988@users.noreply.github.com>
2020-12-30 11:39:52 +0200

    Update Desktop.ps1

--- powershell/Desktop.ps1
+++ powershell/Desktop.ps1
@@ -3,7 +3,6 @@ REG LOAD HKLM\DEFAULT C:\Users\Default\NTUSER.DAT
 # MyPc shortcut
 reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 
-reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer" /v HideSCAMeetNow /t REG_DWORD /d "1" /f
 reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton /t REG_DWORD /d "0" /f
 reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowCortanaButton /t REG_DWORD /d "0" /f
 
