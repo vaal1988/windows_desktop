9c6f5707ad602f2115bf2cb93414bb1213d72ae9
Gusachenko Valentin <vgusachenko@gmail.com>
2020-12-30 11:59:42 +0200

    Privacy Settings

--- powershell/PrivacySettings.ps1
+++ powershell/PrivacySettings.ps1
@@ -1,3 +1,26 @@
 # Disable Privacy Settings Experience at Sign-in
-
 reg add "HKLM\Software\Policies\Microsoft\Windows\OOBE" /v DisablePrivacyExperience /t REG_DWORD /d 1 /f
+
+
+# Disable Online Speech Recognition
+reg add "HKCU\Software\Microsoft\Speech_OneCore\Settings\OnlineSpeechPrivacy" /v HasAccepted /t REG_DWORD /d 0 /f
+
+# Turn Off Location for this Device 
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" /v Value /t REG_DWORD /d "Deny" /f
+
+# Turn Off Tailored experiences with diagnostic data
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Privacy" /v TailoredExperiencesWithDiagnosticDataEnabled /t REG_DWORD /d 0 /f
+
+# Turn Off Find My Device
+reg add "HKLM\SOFTWARE\Policies\Microsoft\FindMyDevice" /v AllowFindMyDevice /t REG_DWORD /d 0 /f
+
+# Diagnostic Telemetry
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f
+
+# Disable ability to send inking and typing data to Microsoft to improve the language recognition
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\TextInput" /v AllowLinguisticDataCollection /t REG_DWORD /d 0 /f
+
+# Disable Relevant Ads
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo" /v DisabledByGroupPolicy /t REG_DWORD /d 1 /f
+
+
