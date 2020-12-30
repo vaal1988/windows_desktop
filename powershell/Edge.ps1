9597f0142013e900fc12ebbdc3c4ca9b045d7261
Gusachenko Valentin <vgusachenko@gmail.com>
2020-12-30 14:54:31 +0200

    REG ADD

--- powershell/Edge.ps1
+++ powershell/Edge.ps1
@@ -1,2 +1,2 @@
 # Disable Edge shortcut
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v "DisableEdgeDesktopShortcutCreation" /t REG_DWORD /d "1" /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v "DisableEdgeDesktopShortcutCreation" /t REG_DWORD /d "1" /f
