7e34fad97e802224720d73ce8228dd2f150de087
Gusachenko Valentin <vgusachenko@gmail.com>
2020-12-30 13:15:27 +0200

    Keyboard Layout

--- powershell/KeyboardLayoutPreload.ps1
+++ powershell/KeyboardLayoutPreload.ps1
@@ -5,3 +5,8 @@ reg add "HKLM\DEFAULT\Keyboard Layout\Preload" /v "2" /d "00000419" /f
 reg add "HKLM\DEFAULT\Keyboard Layout\Preload" /v "3" /d "00000422" /f  
 
 REG UNLOAD HKLM\DEFAULT
+
+
+reg add "HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" /v "1" /d "00000409" /f 
+reg add "HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" /v "2" /d "00000419" /f 
+reg add "HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" /v "3" /d "00000422" /f 
