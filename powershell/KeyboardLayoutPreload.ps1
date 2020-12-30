e01bc91b52d2401e036c5b583f60145799facfba
Gusachenko Valentin <vgusachenko@gmail.com>
2020-12-30 14:47:30 +0200

    New-ItemProperty

--- powershell/KeyboardLayoutPreload.ps1
+++ powershell/KeyboardLayoutPreload.ps1
@@ -1,12 +1,11 @@
 REG LOAD HKLM\DEFAULT C:\Users\Default\NTUSER.DAT
 
-Set-RegistryKey -Key 'HKLM\DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name 'LanguageList' -Value 'powershell.exe -command "Set-WinUserLanguageList -LanguageList en-US, ru, uk -Force"' -Type String -ContinueOnError:$True
-
-REG UNLOAD HKLM\DEFAULT
-
-
 $regPath  = "HKLM:\DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
 $regKey   = "LanguageList"
 $regValue = 'powershell.exe -command "Set-WinUserLanguageList -LanguageList en-US, ru, uk -Force"'
  
 New-ItemProperty -Path $regPath -Name $regKey -Value $regValue -PropertyType STRING -Force | Out-Null
+
+REG UNLOAD HKLM\DEFAULT
+
+
