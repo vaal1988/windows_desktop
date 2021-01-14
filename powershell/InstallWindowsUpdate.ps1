e95cd0f2c41984a4bd0bf6c1d696c714d321e6ea
vaal1988 <45043510+vaal1988@users.noreply.github.com>
2021-01-14 18:30:58 +0200

    Update InstallWindowsUpdate.ps1

--- powershell/InstallWindowsUpdate.ps1
+++ powershell/InstallWindowsUpdate.ps1
@@ -1,6 +1,6 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -Force
-Set-ExecutionPolicy RemoteSigned -Force
+Set-ExecutionPolicy Bypass -Scope Process -Force  # The Process scope only affects the current PowerShell session.
 Import-Module PSWindowsUpdate
 Add-WUServiceManager -ServiceID "7971f918-a847-4430-9279-4a52d1efe18d" -Confirm:$false
 
